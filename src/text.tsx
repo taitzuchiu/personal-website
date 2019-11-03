b874cb6e132ce59624726ba05e323772da3e92c0
Tai Tzu Chiu Samantha Eastwood <taitzuchiu@gmail.com>
2019-11-03 17:43:43 +0000

    More words and some links.

--- src/text.tsx
+++ src/text.tsx
@@ -4,7 +4,9 @@ export const aboutMe = (
   <div>
     <p>
       Hi! I’m Sam Eastwood*. Currently, I’m a third-year undergraduate Computer
-      Science student at the University of York. I’m interested in front-end web
+      Science student at the 
+      <a href="https://www.cs.york.ac.uk/"> University of York</a>. 
+      I’m interested in front-end web
       development (I did a whole internship in it!) but generally, I like
       solving puzzles and coding pretty visual things.
     </p>
@@ -30,6 +32,35 @@ export const aboutMe = (
   </div>
 );
 
-export const experience = (<div>experience</div>);
-export const projects = (<div>projects</div>);
-export const intro = (<div></div>);
+export const experience = (
+  <div>
+    <p>
+      <b>Academic:</b> During my first two years at university, my projects and assessments have mainly been in Python
+      and Java. Other languages I have some knowledge in include: C, Scheme, Haskell, Ada, and Prolog. I also have
+      experience with SQL, both from the databases module and from my A-Level project.
+    </p>
+    <p>
+      <b>Employment:</b> 
+      <div> <i>IBM York Lab, July 2018 - August 2019</i> </div>
+      Front end web development using Typescript with React/Redux. Collaborated in an agile team on
+      a relatively new project (which also happened to be a&nbsp;
+      <a href="http://ktp.innovateuk.org/">KTP</a>).
+      Also participated in various IBM events, such as the global&nbsp;
+      <a href="https://developer.ibm.com/callforcode/">Call for Code</a> hackathon challenge
+       (in which
+      my team won a prize, in both years we entered!)
+    </p>
+  </div>
+);
+
+export const projects = (
+  <div>
+    <p>Things I have done / am doing ...</p>
+    <ul>
+      <li>This website! It's a work in progress. </li>
+      <li>I have plans for creating a blog website for baking recipes...</li>
+      <li>And for creating a countdown extension tab in Chrome...</li> 
+    </ul>
+    <p>Stay tuned for hopefully some updates!</p>
+  </div>
+);
